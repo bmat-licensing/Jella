8b68b876dfafe2b6df77fed0a7cafea905fb5f29
Harrington Joseph <h.josephv@gmail.com>
2011-05-20 16:47:03 +0200

    Tests for Track Search

--- src/test/java/com/bmat/ella/JellaTest.java
+++ src/test/java/com/bmat/ella/JellaTest.java
@@ -1,24 +1,74 @@
 package com.bmat.ella;
 
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
-import static org.junit.Assert.*;
 
 public class JellaTest {
 
-  private Jella jella;
+	private Jella jella;
 
-  @Before public void setUp() {
-    jella = new Jella("http://ella.bmat.me/");
+	@Before public void setUp() {
+		try{
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			System.out.print("WS Username: ");
+			String username = br.readLine();
+			System.out.print("WS Password: ");
+			String password = br.readLine();
+			jella = new Jella("http://ella.bmat.ws/", username, password);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	  
   }
 
   @After public void tearDown() {
     // nothing to tear down
   }
 
-  @Test public void testApp() {
-    String id = jella.getArtistId("the rolling stones");
-    assertEquals(id, "abc123");
+  @Test public void testSearchTrack() throws Exception {
+	  // TEST 1 - search
+	  String query1 = "Beautiful day";
+	  String collection1 = "bmat";
+	  String method1 = "search";
+	  String artist1 = "u2";
+	  String album1 = "leave behind";
+	  ArrayList<Track> tracksResult1 = jella.searchTrack(method1, query1, collection1, true, null, new String[]{"artist:" + artist1, "release:" + album1}, null);
+	  for(Track track : tracksResult1){
+		  assertTrue(track.getArtist().getName().toLowerCase().indexOf(artist1.toLowerCase()) != -1);
+		  assertTrue(track.getTitle().toLowerCase().indexOf(query1.toLowerCase()) != -1);
+		  assertTrue(track.getCollection().equalsIgnoreCase(collection1));
+	  }
+	  
+	  // TEST 2 - match
+	  String collection2 = "bmat";
+	  String method2 = "match";
+	  String track2 = "Buffalo Soldier";
+	  ArrayList<Track> tracksResult2 = jella.searchTrack(method2, "", collection2, true, null, new String[]{"track:" + track2}, new Long(0));
+	  for(Track track : tracksResult2){
+		  assertTrue(track.getTitle().toLowerCase().indexOf(track2.toLowerCase()) != -1);
+		  assertTrue(track.getCollection().equalsIgnoreCase(collection2));
+	  }
+	  
+	  // TEST 3 - resolve
+	  String collection3 = "bmat";
+	  String method3 = "resolve";
+	  HashMap<String, String> query = new HashMap<String, String>();
+	  query.put("artist", "Red Hot Chili Peppers");
+	  query.put("track", "Otherside");
+	  ArrayList<Track> tracksResult3 = jella.searchTrack(method3, query, collection3, true, null, new String[]{"track:" + track2}, null);
+	  for(Track track : tracksResult3){
+		  assertTrue(track.getTitle().toLowerCase().indexOf(query.get("track").toString().toLowerCase()) != -1);
+		  assertTrue(track.getArtist().getName().toLowerCase().indexOf(query.get("artist").toString().toLowerCase()) != -1);
+		  assertTrue(track.getCollection().equalsIgnoreCase(collection3));
+	  }
   }
 }
