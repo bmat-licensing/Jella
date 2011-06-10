02d3f033dabe57eee5fa588c14397f77e91706d3
Harrington Joseph <h.josephv@gmail.com>
2011-06-10 15:51:36 +0200

    Artist search refactoring

--- src/main/java/com/bmat/ella/ArtistSearch.java
+++ src/main/java/com/bmat/ella/ArtistSearch.java
@@ -1,122 +1,116 @@
+
 package com.bmat.ella;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-
 import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+/**
+ * Java Class ArtistSearch.
+ * Search for Artists.
+ * @author Harrington Joseph (Harph)
+ * */
+public class ArtistSearch extends Search {
 
-public class ArtistSearch extends Search{
+    /**
+     * Class constructor.
+     * @param ellaConnection A connection to the Ella web service.
+     * @param method A String that contains the name of the type of search
+     * (search, resolve, match).
+     * @param query A string that contains the value of the query.
+     * @param collection The name of the queried collection.
+     * @param fuzzy A Boolean value that indicates if it is a fuzzy search.
+     * @param threshold The lowest Track score to consider. In case of null,
+     * it will be considered as 0.
+     * */
+    public ArtistSearch(final EllaConnection ellaConnection,
+            final String method, final String query,
+            final String collection, final boolean fuzzy,
+            final Double threshold) {
+        super(ellaConnection, collection);
+        this.fuzzy = fuzzy;
+        if (threshold != null) {
+            this.threshold = threshold;
+        } else {
+            this.threshold = 0.0;
+        }
+        this.searchTerms = new HashMap<String, String>();
+        this.metadataLinks = new String[]{"official_homepage_artist_url",
+                "wikipedia_artist_url", "lastfm_artist_url",
+                "myspace_artist_url", "spotify_artist_url",
+        "discogs_artist_url"};
+        this.metadata = "name,relevance,recommendable,artist_decades1,"
+            + "artist_decades2,artist_location,artist_latlng,"
+            + "artist_popularity,musicbrainz_artist_id,"
+            + Util.joinArray(this.metadataLinks, ",");
+        String mtd = "/artists/";
+        if (method.equals("resolve")) {
+            mtd += "resolve";
+            searchTerms.put("artist", query);
+            searchTerms.put("limit", "100");
+            searchTerms.put("fetch_metadata", this.metadata);
+        } else if (!this.fuzzy) { // it's a search
+            if (method.equals("search") || method.equals("match")) {
+                mtd += method;
+            } else {
+                mtd += "search";
+            }
+            searchTerms.put("q", query);
+            searchTerms.put("limit", "100");
+            searchTerms.put("fetch_metadata", this.metadata);
+        } else {
+            mtd += "match";
+            searchTerms.put("q", query);
+            searchTerms.put("limit", "30");
+            searchTerms.put("fuzzy", "true");
+        }
+        searchTerms.put("fetch_metadata", this.metadata);
+        this.method = mtd + this.RESPONSE_TYPE;
+    }
 
-	public ArtistSearch(EllaConnection ellaConnection, String method, String query, String collection, boolean fuzzy, Double threshold) {
-		super(ellaConnection, collection);
-		this.fuzzy = false;
-		this.threshold = threshold != null ? threshold : 0.0;
-	    this.searchTerms = new HashMap<String, String>();
-		this.metadataLinks = new String[]{"official_homepage_artist_url","wikipedia_artist_url","lastfm_artist_url","myspace_artist_url","spotify_artist_url","discogs_artist_url"};
-		this.metadata = "name,relevance,recommendable,artist_decades1,artist_decades2,artist_location,artist_latlng,artist_popularity,musicbrainz_artist_id," + Util.joinArray(this.metadataLinks, ",");
-	    
-	    String mtd = "/artists/";
-		
-		if(method.equals("resolve")){
-			mtd += "resolve";
-			searchTerms.put("artist", query);
-			searchTerms.put("limit", "100");
-			searchTerms.put("fetch_metadata", this.metadata);
-		}
-		else if(!this.fuzzy){// it's a search
-			mtd += method.equals("search") || method.equals("match")? method : "search";
-			searchTerms.put("q", query);
-			searchTerms.put("limit", "100");
-			searchTerms.put("fetch_metadata", this.metadata);
-		}
-		else{
-			mtd += "match";
-			searchTerms.put("q", query);
-			searchTerms.put("limit", "30");
-			searchTerms.put("fuzzy", "true");
-			this.fuzzy = true;
-		}
-		searchTerms.put("fetch_metadata", this.metadata);
-		this.method = mtd + this.RESPONSE_TYPE;
-	}
-	
-	
-	public ArrayList<Artist> getPage(long pageIndex) throws ServiceException, IOException {
-		pageIndex = pageIndex > 0 ? pageIndex - 1 : 0;
-		return this.getResults(this.retrievePage(pageIndex));
-	}
-	
-	public ArrayList<Artist> getNextPage() throws ServiceException, IOException {
-		return this.getResults(this.retrieveNextPage());
-	} 
-	
-	private ArrayList<Artist> getResults(JSONArray jsonResults){
-		if(jsonResults == null)
-			return null;
-		ArrayList<Artist> results = new ArrayList<Artist>();
-		for(Object json : jsonResults){
-			JSONObject jsonArtist = (JSONObject) json;
-			JSONObject jsonEntity = (JSONObject) jsonArtist.get("entity");
-			JSONObject jsonMetadata = (JSONObject) jsonEntity.get("metadata");
-			
-			
-			String artistId = (String) jsonEntity.get("id");
-			if(artistId == null || artistId.trim().equals(""))
-				continue;
-			
-			Object relevance = jsonArtist.get("score");
-			if(relevance == null || relevance.toString().equals(""))
-				continue;
-			
-			double score = new Double(relevance.toString());
-			if(this.fuzzy && score < 0.4)
-				continue;
-    		if(this.method.indexOf("resolve") != -1 && score < this.threshold)
-    			continue;
-    		else if(this.method.indexOf("match") != -1 && score < this.threshold)
-    			continue;
-			
-			String collection = (String) jsonEntity.get("collection");
-			
-			Artist artist = new Artist(this.request.getEllaConnection(), artistId, collection);
-			artist.setName((String)jsonMetadata.get("name"));
-			if (artist.getName() == null)
-				continue;
-			
-			artist.setMbid((String)jsonMetadata.get("musicbrainz_artist_id"));
-			
-			Object apop = jsonMetadata.get("artist_popularity");
-			double artistPopularity = apop != null && !apop.toString().equals("") ? new Double(apop.toString()) : 0.0;
-			artist.setPopularity(artistPopularity);
-			
-			String artistLocation = (String) jsonMetadata.get("artist_location");
-			artistLocation = artistLocation != null ? artistLocation : "";
+    /**
+     * @param pageIndex page number.
+     * @return An ArrayList of artists that match the search that are on the
+     * pageIndex.
+     * @throws IOException When there is a problem with the
+     * connection to Ella WS.
+     * @throws ServiceException When Ella WS response fails.
+     * */
+    public final ArrayList<Artist> getPage(final long pageIndex)
+    throws ServiceException, IOException {
+        long pageNumber;
+        if (pageIndex > 0) {
+            pageNumber = pageIndex - 1;
+        } else {
+            pageNumber = 0;
+        }
+        return this.getResults(this.retrievePage(pageNumber));
+    }
 
-			
-			Object latlng = jsonMetadata.get("artist_latlng");
-			if(latlng instanceof String)
-				artist.setLatlng(latlng.toString().trim());
-			else if(latlng instanceof JSONArray)
-				artist.setLatlng((JSONArray)latlng);
-			
-			
-			
-			for(String link : this.metadataLinks){
-				Object linkObject = jsonMetadata.get(link);
-				if(linkObject instanceof String)
-					artist.setLinks(link, (String)jsonMetadata.get(link));
-				else if(linkObject !=null)
-					artist.setLinks(link, (JSONArray) jsonMetadata.get(link));
-			}
-			
+    /**
+     * @return An ArrayList of artists that match the search that are on the
+     * next page.
+     * @throws IOException When there is a problem with the
+     * connection to Ella WS.
+     * @throws ServiceException When Ella WS response fails.
+     * */
+    public final ArrayList<Artist> getNextPage()
+    throws ServiceException, IOException {
+        return this.getResults(this.retrieveNextPage());
+    }
 
-			Object recommend = jsonMetadata.get("recommendable");
-			artist.setRecommend(recommend);
-			results.add(artist);
-		}
-		return results;
-	}
-	
+    /**
+     * @param jsonResults the result node of the response.
+     * @return An ArrayList of the artists contained in the jsonResults.
+     * */
+    private ArrayList<Artist> getResults(final JSONArray jsonResults) {
+        if (jsonResults == null) {
+            return null;
+        }
+        ArrayList<Artist> results = new ArtistManager().getArtists(
+                this.request.getEllaConnection(), jsonResults,
+                this.method, this.threshold, this.fuzzy,
+                this.metadataLinks);
+        return results;
+    }
 }
