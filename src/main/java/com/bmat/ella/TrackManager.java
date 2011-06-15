75959a344188e2ac621579cfb72952f52cccfd57
Harrington Joseph <h.josephv@gmail.com>
2011-06-15 20:57:07 +0200

    Track search refactoring

--- src/main/java/com/bmat/ella/TrackManager.java
+++ src/main/java/com/bmat/ella/TrackManager.java
@@ -20,7 +20,7 @@ public class TrackManager {
      * */
     public final ArrayList<Track> getTracks(final EllaConnection ellaConnection,
             final JSONArray results) {
-        return this.getTracks(ellaConnection, results, null);
+        return this.getTracks(ellaConnection, results, null, null, null);
     }
 
     /**
@@ -34,6 +34,50 @@ public class TrackManager {
      * */
     public final ArrayList<Track> getTracks(final EllaConnection ellaConnection,
             final JSONArray results, final Double threshold) {
+        return this.getTracks(ellaConnection, results, null, null, threshold);
+    }
+
+    /**
+     * Extract all the data from the JSONArray and create an ArrayList of
+     * Track objects.
+     * @param ellaConnection A connection to the Ella web service.
+     * @param results The JSONArray that contains the WS response.
+     * @param album The Album instance associated to the tracks.
+     * @return An ArrayList of Track objects created from the JSONArray data.
+     * */
+    public final ArrayList<Track> getTracks(final EllaConnection ellaConnection,
+            final JSONArray results, final Album album) {
+        return this.getTracks(ellaConnection, results,
+                album, album.getArtist(), null);
+    }
+
+    /**
+     * Extract all the data from the JSONArray and create an ArrayList of
+     * Track objects.
+     * @param ellaConnection A connection to the Ella web service.
+     * @param results The JSONArray that contains the WS response.
+     * @param artist The Artist instance associated to the tracks.
+     * @return An ArrayList of Track objects created from the JSONArray data.
+     * */
+    public final ArrayList<Track> getTracks(final EllaConnection ellaConnection,
+            final JSONArray results, final Artist artist) {
+        return this.getTracks(ellaConnection, results, null, artist, null);
+    }
+
+    /**
+     * Extract all the data from the JSONArray and create an ArrayList of
+     * Track objects.
+     * @param ellaConnection A connection to the Ella web service.
+     * @param results The JSONArray that contains the WS response.
+     * @param alb The Album instance associated to the tracks.
+     * @param art The Artist instance associated to the tracks.
+     * @param threshold The lowest Track score to consider. In case of null,
+     * it will be considered as 0.
+     * @return An ArrayList of Track objects created from the JSONArray data.
+     * */
+    public final ArrayList<Track> getTracks(final EllaConnection ellaConnection,
+            final JSONArray results, final Album alb,
+            final Artist art, final Double threshold) {
         ArrayList<Track> tracks = new ArrayList<Track>();
         for (Object json : results) {
             JSONObject jsonTrack = (JSONObject) json;
@@ -59,36 +103,43 @@ public class TrackManager {
             if (artistId == null) {
                 continue;
             }
-
-            Artist artist = new Artist(ellaConnection, artistId,
-                    entityCollection);
-            artist.setName((String) jsonMetadata.get("artist"));
-            if (artist.getName() == null) {
-                continue;
-            }
-            Object apop = jsonEntity.get("artist_popularity");
-            double artistPopularity = 0.0;
-            if (apop != null && !apop.toString().equals("")) {
-                artistPopularity = new Double(apop.toString());
-            }
-            artist.setPopularity(artistPopularity);
-
-            Object artistLocation = jsonMetadata.get("artist_location");
-            if (artistLocation != null && !artistLocation.toString()
-                    .equals("")) {
-                artist.setLocation(artistLocation.toString());
-            } else {
-                artist.setLocation("");
-            }
             Object recommend = jsonMetadata.get("recommendable");
-            artist.setRecommend(recommend);
 
             Track track = new Track(ellaConnection, trackId, entityCollection);
             track.setTitle((String) jsonMetadata.get("track"));
             track.setAudio((String) jsonMetadata.get("location"));
-            track.setArtist(artist);
-            track.setArtistName(artist.getName());
-            track.setArtistId(artist.getId());
+
+            if (art == null) {
+                Artist artist = new Artist(ellaConnection, artistId,
+                        entityCollection);
+                artist.setName((String) jsonMetadata.get("artist"));
+                if (artist.getName() == null) {
+                    continue;
+                }
+                Object apop = jsonEntity.get("artist_popularity");
+                double artistPopularity = 0.0;
+                if (apop != null && !apop.toString().equals("")) {
+                    artistPopularity = new Double(apop.toString());
+                }
+                artist.setPopularity(artistPopularity);
+
+                Object artistLocation = jsonMetadata.get("artist_location");
+                if (artistLocation != null && !artistLocation.toString()
+                        .equals("")) {
+                    artist.setLocation(artistLocation.toString());
+                } else {
+                    artist.setLocation("");
+                }
+                artist.setRecommend(recommend);
+                track.setArtist(artist);
+                track.setArtistName(artist.getName());
+                track.setArtistId(artist.getId());
+            } else {
+                track.setArtist(art);
+                track.setArtistName(art.getName());
+                track.setArtistId(art.getId());
+            }
+
             track.setRecommend(recommend);
 
             Object tpop = jsonMetadata.get("track_popularity");
@@ -104,18 +155,23 @@ public class TrackManager {
             } else if (trackSmallImages != null) {
                 track.setImages((JSONArray) trackSmallImages);
             }
-
-            Object albumId = jsonMetadata.get("release_service_id");
-            if (albumId != null) {
-                Album album = new Album(ellaConnection, (String) albumId,
-                        entityCollection);
-                album.setTitle((String) jsonMetadata.get("release"));
-                album.setArtist(artist);
-                album.setImage((String) jsonMetadata.get(
-                        "release_small_image"));
-                track.setAlbum(album);
-                track.setAlbumTitle(album.getTitle());
-                track.setAlbumId(album.getId());
+            if (alb == null) {
+                Object albumId = jsonMetadata.get("release_service_id");
+                if (albumId != null) {
+                    Album album = new Album(ellaConnection, (String) albumId,
+                            entityCollection);
+                    album.setTitle((String) jsonMetadata.get("release"));
+                    album.setArtist(track.getArtist());
+                    album.setImage((String) jsonMetadata.get(
+                            "release_small_image"));
+                    track.setAlbum(album);
+                    track.setAlbumTitle(album.getTitle());
+                    track.setAlbumId(album.getId());
+                }
+            } else {
+                track.setAlbum(alb);
+                track.setAlbumTitle(alb.getTitle());
+                track.setAlbumId(alb.getId());
             }
             tracks.add(track);
         }
